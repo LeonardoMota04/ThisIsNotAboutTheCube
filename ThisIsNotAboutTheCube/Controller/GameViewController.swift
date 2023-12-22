d556fb8971ae3e85605c4c35709b97ee42ea67a7
Leonardo Pereira Mota <leomotadf@gmail.com>
2023-12-21 22:41:21 -0300

    Nao entendi nada

--- ThisIsNotAboutTheCube/Controller/GameViewController.swift
+++ ThisIsNotAboutTheCube/Controller/GameViewController.swift
@@ -43,6 +43,22 @@ class ViewController: UIViewController {
         rubiksCube = RubiksCube()
         rootNode.addChildNode(rubiksCube)
         
+        let shaderUrl = URL(fileURLWithPath: Bundle.main.path(forResource: "cel", ofType: "shader")!)
+        
+        var data:String!
+        do {
+            data = try String(contentsOf: shaderUrl, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print("Error: \(error.localizedDescription)")
+        }
+        var shaders = [SCNShaderModifierEntryPoint:String]()
+        shaders[SCNShaderModifierEntryPoint.fragment] = data
+        
+        let mod = "float flakeSize = sin(u_time * 0.2);\n" + "float flakeIntensity = 0.7;\n" + "vec3 paintColor0 = vec3(0.9, 0.4, 0.3);\n" + "vec3 paintColor1 = vec3(0.9, 0.75, 0.2);\n" + "vec3 flakeColor = vec3(flakeIntensity, flakeIntensity, flakeIntensity);\n" + "vec3 rnd =  texture2D(u_diffuseTexture, _surface.diffuseTexcoord * vec2(1.0) * sin(u_time*0.1) ).rgb;\n" + "vec3 nrm1 = normalize(0.05 * rnd + 0.95 * _surface.normal);\n" + "vec3 nrm2 = normalize(0.3 * rnd + 0.4 * _surface.normal);\n" + "float fresnel1 = clamp(dot(nrm1, _surface.view), 0.0, 1.0);\n" + "float fresnel2 = clamp(dot(nrm2, _surface.view), 0.0, 1.0);\n" + "vec3 col = mix(paintColor0, paintColor1, fresnel1);\n" + "col += pow(fresnel2, 106.0) * flakeColor;\n" + "_surface.normal = nrm1;\n" + "_surface.diffuse = vec4(col.r,col.b,col.g, 1.0);\n" + "_surface.emission = (_surface.reflective * _surface.reflective) * 2.0;\n" + "_surface.reflective = vec4(0.0);\n"
+        
+        shaders[SCNShaderModifierEntryPoint.surface] = mod
+        rootNode.geometry?.firstMaterial?.shaderModifiers = shaders
+        
         // Criando a animação de flutuação
         let floatUp = SCNAction.move(by: SCNVector3(0, 0.3, 0), duration: 1.0)
         let floatDown = SCNAction.move(by: SCNVector3(0, -0.3, 0), duration: 1.0)
